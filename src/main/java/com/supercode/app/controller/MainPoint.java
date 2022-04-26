b3b2be6242de84c0360a12b26a928c92f4946642
mpietal <mateusz.pietal@external.thalesgroup.com>
2022-04-26 10:22:23 +0200

    Initial drawing.

--- src/main/java/com/supercode/app/controller/MainPoint.java
+++ src/main/java/com/supercode/app/controller/MainPoint.java
@@ -1,10 +1,13 @@
 package com.supercode.app.controller;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -15,21 +18,27 @@ public class MainPoint extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.show();
-        Canvas canvas = new Canvas();
+        Canvas canvas = new Canvas(300,300);
 
         Group group = new Group();
         Scene scene = new Scene(group, 100, 100);
         stage.setScene(scene);
         group.getChildren().add(canvas);
 
-        GraphicsContext graphicsContext2D = canvas.getGraphicsContext2D();
-        PixelWriter pixelWriter = graphicsContext2D.getPixelWriter();
-        pixelWriter.setColor(0,0, Color.RED);
-        pixelWriter.setColor(1,0, Color.RED);
-        pixelWriter.setColor(2,0, Color.RED);
-        pixelWriter.setColor(3,0, Color.RED);
+
+        setColors(canvas);
     }
 
+    private void setColors(Canvas canvas) {
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        PixelWriter pixelWriter = gc.getPixelWriter();
+        for (int i=0; i<100; i++){
+            pixelWriter.setColor(i,0, Color.RED);
+        }
+        for (int i=0; i<100; i++){
+            pixelWriter.setColor(i,3, Color.YELLOW);
+        }
+    }
 
 
     public static void main(String[] args) {
